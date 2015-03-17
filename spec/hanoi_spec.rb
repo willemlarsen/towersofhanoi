bfc89e05f936f11b771c43489f642de78135f495
Willem Larsen <willemlarsen@gmail.com>
2015-03-17 15:48:42 +0000

    Second test: motivate de-fake by extending behaviour

--- spec/hanoi_spec.rb
+++ spec/hanoi_spec.rb
@@ -4,4 +4,11 @@ describe "A solver for the Towers Of Hanoi problem" do
   it "can solve the problem for a one-disk high stack" do
     expect(hanoi_solution(1)).to eq([{from: 1, to: 3}])
   end
+
+  it "can solve the problem for a two-disc high stack" do
+    expect(hanoi_solution(2)).to eq([
+      {from: 1, to: 2},
+      {from: 1, to: 3},
+      {from: 2, to: 3}])
+  end
 end
