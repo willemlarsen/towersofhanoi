8910d4e67b7489e918a3ef1baffc0adcb2d5d37d
Willem Larsen <willemlarsen@gmail.com>
2015-03-17 16:23:08 +0000

    Push fake one level down: introduce getSparePeg

--- lib/hanoi.rb
+++ lib/hanoi.rb
@@ -6,8 +6,12 @@ def move_n_discs(n, from_to)
   return [from_to] if n == 1
 
   oneLessThatIsCurrentlyOne = n - 1 # equals one because test n = 2
-
-  move_n_discs(oneLessThatIsCurrentlyOne, {from: 1, to: 2}) + # [{from: 1, to: 2}]
+  sparePeg = getSparePeg(from_to)
+  move_n_discs(oneLessThatIsCurrentlyOne, {from: 1, to: sparePeg}) + # [{from: 1, to: 2}]
   move_n_discs(1, {from: 1, to: 3}) +                         # [{from: 1, to: 3}]
-  move_n_discs(oneLessThatIsCurrentlyOne, {from: 2, to: 3})   # [{from: 2, to: 3}]
+  move_n_discs(oneLessThatIsCurrentlyOne, {from: sparePeg, to: 3})   # [{from: 2, to: 3}]
+end
+
+def getSparePeg(from_to)
+  2
 end
