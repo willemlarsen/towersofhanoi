de9677c5980114d322b83f393371671d4aa7fe3b
Willem Larsen <willemlarsen@gmail.com>
2015-03-17 15:37:08 +0000

    First test: introduce API

--- spec/hanoi_spec.rb
+++ spec/hanoi_spec.rb
@@ -1,5 +1,5 @@
 describe "A solver for the Towers Of Hanoi problem" do
-  it "can produce a red (failing) bar" do
-    expect(1).to eq(2)
+  it "can solve the problem for a one-disk high stack" do
+    expect(hanoi_solution(1)).to eq([{from: 1, to: 3}])
   end
 end
